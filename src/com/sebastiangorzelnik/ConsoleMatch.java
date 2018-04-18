c431660b3c49260bc1195b91dd616c510be0e1ba
Your Name <you@example.com>
2018-04-18 08:52:30 +0200

    Commit before pushing to GitHub

---
+++ src/com/sebastiangorzelnik/ConsoleMatch.java
@@ -0,0 +1,99 @@
+package com.sebastiangorzelnik;
+
+import java.util.Scanner;
+
+public class ConsoleMatch {
+    String playerOne;
+    String playerTwo;
+    int playerOneScore;
+    int playerTwoScore;
+    Scanner in = new Scanner(System.in);
+
+    public ConsoleMatch(String playerOne, String playerTwo, int playerOneScore, int playerTwoScore, int howManyFields) {
+        playMatch(playerOne, playerTwo, playerOneScore, playerTwoScore, howManyFields);
+
+
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.playerOneScore = playerOneScore;
+        this.playerTwoScore = playerTwoScore;
+    }
+
+    private void playMatch(String playerOne, String playerTwo, int playerOneScore, int playerTwoScore, int howManyFields) {
+        boolean isEnd = false;
+        char[][] board = new char[howManyFields][howManyFields];
+        int counter = 0;
+        int column;
+        int row;
+        while (!isEnd) {
+            System.out.println("Please insert O, on free column (1-" + howManyFields + ") and row (1-" + howManyFields + ")");
+            showBoard(board);
+            column = in.nextInt() - 1;
+            row = in.nextInt() - 1;
+            counter++;
+            if (ifEnd(counter, howManyFields)) {
+                break;
+            }
+
+            enterSymbol('O', board, column, row);
+            showBoard(board);
+
+
+            System.out.println("Please insert X, on free column (0-" + howManyFields + ") and row (0-" + howManyFields + ")");
+            column = in.nextInt() - 1;
+            row = in.nextInt() - 1;
+            counter++;
+            if (ifEnd(counter, howManyFields)) {
+                break;
+            }
+            enterSymbol('X', board, column, row);
+
+
+        }
+    }
+
+    private void showBoard(char board[][]) {
+        for (int i = 0; board.length > i; i++)
+            for (int j = 0; board[i].length > j; j++) {
+                if (j == 0)
+                    System.out.print(printSymbol(board[i][j]));
+                else if (j == board[i].length - 1)
+                    System.out.println(printSymbol(board[i][j]));
+                else
+                    System.out.print("|" + printSymbol(board[i][j]) + "|");
+
+
+            }
+    }
+
+    private String printSymbol(char symbol) {
+        String output;
+        if (symbol == 'O' || symbol == 'X')
+            output = " " + symbol + " ";
+        else
+            output = "   ";
+
+        return output;
+    }
+
+    private void enterSymbol(char symbol, char board[][], int column, int row) {
+        if (board[column][row] != 0) {
+            System.out.println("This field is already filled, plase choose again");
+            column = in.nextInt() - 1;
+            row = in.nextInt() - 1;
+            enterSymbol(symbol, board, column, row);
+        } else {
+            board[column][row] = symbol;
+        }
+    }
+
+    private boolean ifEnd(int counter, int howManyFields) {
+        counter *= counter;
+        if (counter == howManyFields)
+            return true;
+        else
+            return false;
+    }
+}
+
+
--- src/com/sebastiangorzelnik/Main.java
+++ src/com/sebastiangorzelnik/Main.java
@@ -1,8 +1,29 @@
 package com.sebastiangorzelnik;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Plase enter a Player One name: ");
+        String playerOneName = in.nextLine();
+        System.out.println("Plase enter a Player Two name: ");
+        String playerTWoName = in.nextLine();
+        int playerOneScore = 0;
+        int playerTwoScore = 0;
+        System.out.println("How many fields should have a board?:");
+        int field = in.nextInt();
+        ConsoleMatch FirstMatch = new ConsoleMatch(playerOneName, playerTWoName,playerOneScore, playerTwoScore, field);
+
+
+     /*   int [][] board = new int [3][3];
+        for(int i =0; board.length>i; i++)
+            for (int j =0; board[i].length >j ; j++) {
+                System.out.print(j);
+                if (j+1==board[i].length)
+                    System.out.println();
+
+            }
+    */
     }
+
 }
